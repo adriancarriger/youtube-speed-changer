3788e119daaad360b106341a1fb10ee34d993192
Adrian Carriger <mail@adrianandnicole.com>
2017-01-14 11:37:43 -0800

    feat(script): move controls to hidden toggle box

--- src/script.ts
+++ src/script.ts
@@ -67,20 +67,40 @@
     v.video = document.querySelectorAll('video')[0];
     const referenceNode = document.getElementById('watch-header');
     const newNode = document.createElement('div');
-    newNode.setAttribute('class', 'yt-uix-expander yt-card yt-card-has-padding');
+    newNode.setAttribute('class', 'yt-uix-expander yt-card yt-card-has-padding yt-uix-expander-collapsed');
     newNode.innerHTML = `
       <h1 style='padding-top: 5px;'>Youtube Speed Changer Extension</h1>
       <div>Speed: <span id='ytsc-percent'>100%</span></div>
       <div>Start point: <span id='ytsc-start'>0</span></div>
-      <h2 style='padding-top: 10px;'>Keyboard Controls</h2>
-      <ul>
-        <li><b>v</b>: Toggles start/stop (from start point)
-        <li><b>s</b>: Slow down 5%
-        <li><b>d</b>: Speed up 5%
-        <li><b>t</b>: Set start point to current video position
-        <li><b>q</b>: Move start point backward 1 second
-        <li><b>r</b>: Move start point forward 1 second</li>
-      </ul>`;
+      <div class="yt-uix-expander-body">
+        <h2 style='padding-top: 10px;'>Basic Controls</h2>
+        <ul>
+          <li><b>v</b>: Toggles start/stop (from start point)
+          <li><b>s</b>: Slow down 5%
+          <li><b>d</b>: Speed up 5%
+          <li><b>t</b>: Set start point to current video position
+          <li><b>q</b>: Move start point backward 1 second
+          <li><b>r</b>: Move start point forward 1 second</li>
+        </ul>
+        <h2 style='padding-top: 10px;'>Precision Controls</h2>
+        <ul>
+          <li><b>x</b>: Slow down 1%
+          <li><b>c</b>: Speed up 1%
+          <li><b>w</b>: Move start point backward 0.1 seconds
+          <li><b>e</b>: Move start point forward 0.1 seconds
+        </ul>
+      </div>
+      <button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander
+        yt-uix-expander-head yt-uix-expander-collapsed-body yt-uix-gen204"
+        type="button" onclick=";return false;"
+        data-gen204="feature=watch-show-more-metadata">
+        <span class="yt-uix-button-content">Show more</span>
+      </button>
+      <button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander
+        yt-uix-expander-head yt-uix-expander-body" type="button"
+        onclick=";return false;">
+        <span class="yt-uix-button-content">Show less</span>
+      </button>`;
     const videoPlayer = <HTMLElement>referenceNode
       .parentNode
       .insertBefore(newNode, referenceNode);
