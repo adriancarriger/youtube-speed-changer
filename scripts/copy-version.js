e25c8905270cf74fe8050e2e201bb645410109c3
Adrian Carriger <mail@adrianandnicole.com>
2017-01-28 17:33:02 -0800

    chore(build): update manifest on release

---
+++ scripts/copy-version.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const version = require('../package.json').version;
+const mPath = path.join(__dirname, '../src/manifest.json');
+const manifest = require(mPath);
+
+if (version !== manifest.version) {
+  manifest.version = version;
+  fs.writeFile(mPath, JSON.stringify(manifest), err => {
+    if (err) return console.log(err);
+  });
+}
