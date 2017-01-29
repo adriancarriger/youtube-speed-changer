cd16d216bb0d080cd5380d8bbd8e01cedd7c8b15
Adrian Carriger <mail@adrianandnicole.com>
2017-01-29 15:31:05 -0800

    test(controller): test speed changes and toggle

--- tests/youtube-controller.spec.ts
+++ tests/youtube-controller.spec.ts
@@ -25,6 +25,32 @@ describe('Controller: YoutubeController', () => {
     keypress(115); // key: s
     expect( speed(youtubeVideo) ).toEqual(95);
   });
+
+  it('should speed up the video by 5%', () => {
+    expect( speed(youtubeVideo) ).toEqual(100);
+    keypress(100); // key: d
+    expect( speed(youtubeVideo) ).toEqual(105);
+  });
+
+  it('should slow down the video by 1%', () => {
+    expect( speed(youtubeVideo) ).toEqual(100);
+    keypress(120); // key: x
+    expect( speed(youtubeVideo) ).toEqual(99);
+  });
+
+  it('should speed up the video by 1%', () => {
+    expect( speed(youtubeVideo) ).toEqual(100);
+    keypress(99); // key: c
+    expect( speed(youtubeVideo) ).toEqual(101);
+  });
+
+  it('should toggle play/pause', () => {
+    expect( youtubeVideo.playing ).toEqual(true);
+    keypress(118); // key: v
+    expect( youtubeVideo.playing ).toEqual(false);
+    keypress(118); // key: v
+    expect( youtubeVideo.playing ).toEqual(true);
+  });
 });
 
 function keypress(character: number) {
